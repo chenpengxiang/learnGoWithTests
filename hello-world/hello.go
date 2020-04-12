de912f198a7a9c16a5091df0fbf640ddf7025b66
PengXiang Chen <cpengxiang@gmail.com>
2020-04-13 01:16:59 +0800

    practising to refactor Hello function

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,10 +2,30 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+const chinese = "Chinese"
+const french = "French"
+const english = "English"
+const englishHelloPrefix = "Hello, "
+const chineseHelloPrefix = "你好, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	prefix := englishHelloPrefix
+	switch language {
+	case chinese:
+		prefix = chineseHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
