5ad6462eb9b89b5135d3eae72316cf800f671ede
PengXiang Chen <cpengxiang@gmail.com>
2020-04-13 15:22:03 +0800

    pair programming with Roy to add sum test cases

---
+++ arrays_and_slices/sum.go
@@ -0,0 +1,22 @@
+package main
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+
+	return sum
+}
+
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tailNumbers := numbers[1:]
+			sums = append(sums, Sum(tailNumbers))
+		}
+	}
+	return
+}
