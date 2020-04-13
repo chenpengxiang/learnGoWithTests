b51d8ba00c5cacaed57f5814f68b8d1775203f00
PengXiang Chen <cpengxiang@gmail.com>
2020-04-13 14:23:30 +0800

    add one more repeat test case

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -3,12 +3,25 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
-	}
+	t.Run("testing repeat b", func(t *testing.T) {
+		repeated := Repeat("b")
+		expected := "bbbbb"
+
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+
+	t.Run("testing repeat a", func(t *testing.T) {
+		repeated := Repeat("a")
+		expected := "aaaaa"
+
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+
 }
 
 func BenchmarkRepeat(b *testing.B) {
