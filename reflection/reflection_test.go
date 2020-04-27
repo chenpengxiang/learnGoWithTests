b1466653ec96ddfdad47bea0a660f46e724baf3e
PengXiang Chen <cpengxiang@gmail.com>
2020-04-28 01:48:02 +0800

    completed the reflection practices

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -70,6 +70,22 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"Shanghai", "Beijing"},
 		},
+		{
+			"Array",
+			[2] Profile {
+				{33, "Shanghai"},
+				{35, "Beijing"},
+			},
+			[]string{"Shanghai", "Beijing"},
+		},
+		{
+			"Maps",
+			map[string]string {
+				"Foo": "Bar",
+				"Baz": "Boz",
+			},
+			[]string{"Bar", "Boz"},
+		},
 	}
 
 	for _, test := range cases {
@@ -82,5 +98,71 @@ func TestWalk(t *testing.T) {
 				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
 			}
 		})
+	}
+
+	t.Run("with maps", func(t *testing.T){
+		aMap := map[string] string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+
+		var got []string
+		walk(aMap, func(input string){
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Bar")
+		assertContains(t, got, "Boz")
+	})
+
+	t.Run("with channels", func(t *testing.T){
+		aChannel := make(chan Profile)
+
+		go func() {
+			aChannel <- Profile{34, "Shanghai"}
+			aChannel <- Profile{36, "Beijing"}
+			close(aChannel)
+		}()
+
+		var got []string
+		want := []string {"Shanghai", "Beijing"}
+		walk(aChannel, func(input string){
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("with function", func(t *testing.T){
+		aFunction := func() (Profile, Profile) {
+			return Profile{34, "Shanghai"}, Profile{36, "Beijing"}
+		}
+
+		var got []string
+		want := []string{"Shanghai", "Beijing"}
+
+		walk(aFunction, func(input string){
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+}
+
+func assertContains(t *testing.T, haystack []string, needle string) {
+	contains := false
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+			break
+		}
+	}
+
+	if !contains {
+		t.Errorf("expected %+v to contain %q but it didn't", haystack, needle)
 	}
 }
