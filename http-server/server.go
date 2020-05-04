a6f4f8fde31f63555f1891b4c5ab3f87dd12c2ce
PengXiang Chen <cpengxiang@gmail.com>
2020-05-04 10:15:59 +0800

    completed http server tdd practices

--- http-server/server.go
+++ http-server/server.go
@@ -2,9 +2,40 @@ package main
 
 import (
 	"net/http"
+	"strings"
 	"fmt"
 )
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+	RecordWin(name string)
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w, player)
+	case http.MethodGet:
+		p.showScore(w, player)
+	}	
+}
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
+	
+	score := p.store.GetPlayerScore(player)
+	if score == 0 {
+		w.WriteHeader(http.StatusNotFound)
+	}
+	fmt.Fprint(w, score)
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
+	p.store.RecordWin(player)
+	w.WriteHeader(http.StatusAccepted)
 }
